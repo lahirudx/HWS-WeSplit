d9af0c36ccc580d92d63a1fe7daa26e12ac6aa6e
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-15 19:02:32 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lahiru Amarasooriya on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
